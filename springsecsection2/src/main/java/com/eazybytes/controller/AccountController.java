181f00a501cfc692eab5beb1b02330459e04ce20
LeonLow97 <lowjiewei@gmail.com>
2023-05-28 13:40:55 +0800

    Project setup for eazybank

---
+++ springsecsection2/src/main/java/com/eazybytes/controller/AccountController.java
@@ -0,0 +1,14 @@
+package com.eazybytes.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class AccountController {
+
+    @GetMapping(value = "/myAccount")
+    public String getAccountDetails() {
+        return "Here are the account details from the DB!";
+    }
+
+}
