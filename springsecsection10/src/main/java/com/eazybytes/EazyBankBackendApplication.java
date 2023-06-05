749ca9297beb22b16b460d199e5ba434532522e7
LeonLow97 <lowjiewei@gmail.com>
2023-06-05 19:25:31 +0800

    Added PreAuthorize to repository

--- springsecsection10/src/main/java/com/eazybytes/EazyBankBackendApplication.java
+++ springsecsection10/src/main/java/com/eazybytes/EazyBankBackendApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 // @EnableWebSecurity(debug = true) // FOR DEVELOPMENT ONLY
+@EnableMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 @EnableJpaRepositories("com.eazybytes.repository") // optional
 @EntityScan("com.eazybytes.model") // optional
 public class EazyBankBackendApplication {
